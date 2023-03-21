258af18e4c66a23b9a3380083f2a5983460e4087
chaehee-Jin <heeheer_g@naver.com>
2023-03-21 13:49:43 +0900

    user페이지 수정중

---
+++ todo_teample/js/app.js
@@ -0,0 +1,15 @@
+window.onload = () => {
+    AsideEvent.getInstance().addEventShowMenuButton();
+    AsideEvent.getInstance().addEventMainChange();
+    InformationService.getInstance().loadInfo();
+    InformationEvent.getInstance().addEventPhotoChangeClick();
+    InformationEvent.getInstance().addEventPhotoChange();
+
+    InformationEvent.getInstance().addEventModifyAboutMeClick();
+    InformationEvent.getInstance().addEventSaveAboutMeClick();
+    InformationEvent.getInstance().addEventModifyIntroduceClick();
+    InformationEvent.getInstance().addEventSaveIntroduceClick();
+    TodoEvent.getInstnace().addEventAddTodoClick();
+    TodoEvent.getInstnace().addEventAddTodoKeyUp();
+    TodoService.getInstnace();
+}
